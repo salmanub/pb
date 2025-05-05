1fc21de1c08f2e9f779254d67ce4ea1d2e88ab9c
Albert <avilardell@outlook.es>
2025-05-05 09:47:50 +0200

    make git

--- eleventy.config.js
+++ eleventy.config.js
@@ -57,14 +57,14 @@ export default function (eleventyConfig) {
     return `
       <!-- Favicons básicos -->
       ${faviconSVGDataURI ? `<link rel="icon" type="image/svg+xml" href="${faviconSVGDataURI}">` : ''}
-      <link rel="icon" type="image/png" sizes="96x96" href="/assets/icons/favicon-96x96.png">
-      <link rel="icon" type="image/png" sizes="48x48" href="/assets/icons/favicon-48x48.png">
-      <link rel="icon" type="image/png" sizes="32x32" href="/assets/icons/favicon-32x32.png">
-      <link rel="icon" type="image/png" sizes="16x16" href="/assets/icons/favicon-16x16.png">
+      <link rel="icon" type="image/png" sizes="96x96" href="https://perito.barcelona/assets/icons/favicon-96x96.png">
+      <link rel="icon" type="image/png" sizes="48x48" href="https://perito.barcelona/assets/icons/favicon-48x48.png">
+      <link rel="icon" type="image/png" sizes="32x32" href="https://perito.barcelona/assets/icons/favicon-32x32.png">
+      <link rel="icon" type="image/png" sizes="16x16" href="https://perito.barcelona/assets/icons/favicon-16x16.png">
       
       <!-- PWA/Mobile icons -->
-      <link rel="apple-touch-icon" sizes="180x180" href="/assets/icons/apple-touch-icon.png">
-      <link rel="mask-icon" href="/assets/icons/safari-pinned-tab.svg" color="#06b6d4">
+      <link rel="apple-touch-icon" sizes="180x180" href="https://perito.barcelona/assets/icons/apple-touch-icon.png">
+      <link rel="mask-icon" href="https://perito.barcelona/assets/icons/safari-pinned-tab.svg" color="#06b6d4">
       
       <!-- Manifest y configuración del navegador -->
       <link rel="manifest" href="https://perito.barcelona/site.webmanifest">
