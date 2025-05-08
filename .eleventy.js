dcc17478434f05cb299955e4f248a5f6894d631c
Albert <avilardell@outlook.es>
2025-05-08 15:06:27 +0200

    make git

--- .eleventy.js
+++
@@ -1,8 +0,0 @@
-const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
-
-module.exports = function(eleventyConfig) {
-    // Add Navigation plugin
-    eleventyConfig.addPlugin(eleventyNavigationPlugin);
-
-    // Other configurations can go here
-};
