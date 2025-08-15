1a7821a1e1c1434e9fc5b24589b8d3fd43b0c803
Albert <avilardell@outlook.es>
2025-08-15 18:25:41 +0200

    make git

--- tailwind.config.js
+++ tailwind.config.js
@@ -155,6 +155,15 @@ export default function (eleventyConfig) {
           transformStyle: {
             'preserve-3d': 'preserve-3d',
           },
+          animation: {
+            'wiggle': 'wiggle 0.3s ease-in-out',
+          },
+          keyframes: {
+            wiggle: {
+              '0%, 100%': { transform: 'rotate(-3deg)' },
+              '50%': { transform: 'rotate(3deg)' },
+            }
+          }
         }
       },
       safelist: ['group', 'group-hover:text-emerald-600']
